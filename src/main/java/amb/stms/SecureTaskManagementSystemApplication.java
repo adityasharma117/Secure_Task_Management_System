f248488f1119ba71378835559c399d9074788f03
adityasharma117 <karan170105@gmail.com>
2026-09-26 01:41:46 +0530

    Initial commit

---
+++ src/main/java/amb/stms/SecureTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package amb.stms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureTaskManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amb/stms/SecureTaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package amb.stms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
